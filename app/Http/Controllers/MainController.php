4f70de287d447d12fa825c7a8578cd49fe3333ec
Pablo Curumaco <jpablocuru@gmail.com>
2022-05-03 23:30:37 -0600

    Table layout
    - http request made with laravel
    - images resized to 200 width
    - added directive
    - 25 record limit
    - 4th row with background

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,9 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
-
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
+ 
 class MainController extends Controller
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('datetime', function ($expression) {
+            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
+        });
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +28,29 @@ class MainController extends Controller
      */
     public function index()
     {
-        return view('main-table');
+        $client = new Client();
+        try {
+            $response = Http::get('https://appmsds-6aa0.kxcdn.com/content.php?lang=de&json=1&search_text=berlin&currencyiso=EUR');
+
+            $res_decode = json_decode($response->body());
+
+            $content = array_slice($res_decode->content, 0, 25);
+            
+            $data = array();
+            foreach ($content as $key => $product) {
+
+                $my_product = array(
+                    'thumb_url' => $product->thumb_url,
+                    'title' => $product->title,
+                    'price' => $product->price,
+                );
+                array_push($data, $my_product);
+            }
+
+        } catch (\Throwable $th) {
+            $response->throw();
+        }
+        return view('main-table', compact('data'));
     }
 
     /**
--- resources/views/main-table.blade.php
+++ resources/views/main-table.blade.php
@@ -4,7 +4,26 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h3>Hi</h3>
+                <table class="table mt-4">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Thumbnail</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $key => $element)
+                            <tr @if ($key == 3) class="bg-secondary text-white" @endif>
+                                <th scope="row">
+                                    <img src="{{ $element['thumb_url'] }}" width="200" alt="thumb_url">
+                                </th>
+                                <td>{{ $element['title'] }}</td>
+                                <td> @money($element['price']) </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
