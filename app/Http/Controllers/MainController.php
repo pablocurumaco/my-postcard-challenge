d00def17c58fe060ab4a8fc53dae58444c7d3526
Pablo Curumaco <jpablocuru@gmail.com>
2022-05-05 19:36:46 -0600

    price for a Greeting card with Envelope

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
-use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
  
 class MainController extends Controller
@@ -28,27 +27,38 @@ class MainController extends Controller
      */
     public function index()
     {
-        $client = new Client();
         try {
+            // make request for cards
             $response = Http::get('https://appmsds-6aa0.kxcdn.com/content.php?lang=de&json=1&search_text=berlin&currencyiso=EUR');
 
             $res_decode = json_decode($response->body());
-
+            // only take the first 25
             $content = array_slice($res_decode->content, 0, 25);
             
+            // prepare the array that I will pass to the view
             $data = array();
             foreach ($content as $key => $product) {
+                // make another request to get the new price
+                $response_price = Http::post('https://www.mypostcard.com/mobile/product_prices.php?json=1&type=get_postcard_products&currencyiso=EUR&store_id=' . $product->id);
+                $data_price = json_decode($response_price->body());
+
+                // the envelope price
+                $envelope_price = $data_price->products[0]->product_options->Envelope->price;
+                // and my new price
+                $new_price = $envelope_price + $product->price;
 
                 $my_product = array(
                     'thumb_url' => $product->thumb_url,
                     'title' => $product->title,
-                    'price' => $product->price,
+                    'price_greendcard' => $product->price,
+                    'price_envelope' => $envelope_price,
+                    'price' => $new_price,
                 );
                 array_push($data, $my_product);
             }
 
         } catch (\Throwable $th) {
-            $response->throw();
+            dd($th);
         }
         return view('main-table', compact('data'));
     }
